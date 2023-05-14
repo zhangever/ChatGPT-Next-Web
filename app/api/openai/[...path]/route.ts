079406286bc2635b2796b0afc3c8df56eca92e70
老潮 <zhangyongsheng@3vjia.com>
2023-05-14 23:51:33 +0800

    feat: 增加耗时的记录

--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -30,7 +30,7 @@ async function createStream(res: Response, req: NextRequest) {
             );
 
             console.log(
-              `[currentTime][${traceId}][cost]${timeDiff}[Res]${respContent}`,
+              `[${currentTime}][${traceId}][costTime]${timeDiff}[Res]${respContent}`,
             );
             return;
           }
