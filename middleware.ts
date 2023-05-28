a8a8f1c57cd331c7ce6ab984038905ea81d2aa76
老潮 <zhangyongsheng@3vjia.com>
2023-05-28 18:45:30 +0800

    feat:add req log

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,50 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSideConfig } from "./app/config/server";
 import md5 from "spark-md5";
 import { v4 as uuidv4 } from "uuid";
+import moment from "moment-timezone";
+
 
 export const config = {
-  matcher: ["/api/openai", "/api/chat-stream"],
+  matcher: ["/api/openai", "/api/openai/v1/chat/completions"],
 };
 
 const serverConfig = getServerSideConfig();
 
-export async function middleware(req: NextRequest) {
+const IP_HEADERS = [
+  "Magiccube-Req-Ip",
+  "RemoteIp",
+  "X-Real-IP",
+  "X-Forwarded-For",
+  "Proxy-Client-IP",
+  "WL-Proxy-Client-IP",
+  "HTTP_CLIENT_IP",
+  "HTTP_X_FORWARDED_FOR",
+];
 
+function getIP(req: NextRequest) {
+  let ip = "";
+  for (const header of IP_HEADERS) {
+    ip = req.headers.get(header) ?? "";
+    if (ip) {
+      ip = ip.split(",").at(0) ?? "";
+      if (ip) {
+        console.log(`[IP] ${header}: ${ip}`);
+        break;
+      }
+    }
+  }
+
+  return ip;
+}
+
+export async function middleware(req: NextRequest) {
   const traceId = uuidv4();
   req.headers.set("traceId", traceId);
-
+  const now = moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss.SSS');
   const json = await req.json();
-  console.log(`[${req.headers.get("traceId")}}][Req]${JSON.stringify(json.messages)}`);
+  req.headers.set("reqTime", now);
 
+  console.log(`[BIZ][${now}][${req.headers.get("traceId")}}][${getIP(req)}][Req]${JSON.stringify(json.messages)}`);
 
   return NextResponse.next({
     request: {
